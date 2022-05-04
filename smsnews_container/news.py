9001c0790d1360407264205ddb1522d129c56e68
Sweekrithi  S  Shetty <shettysweekrithi03@gmail.com>
2022-05-04 15:20:50 -0400

    Create news.py

---
+++ smsnews_container/news.py
@@ -0,0 +1,13 @@
+from twilio.rest import Client
+
+# Your Account SID from twilio.com/console
+account_sid = "ACda580fcc3fc654fabfee3fe3a1c40244"
+# Your Auth Token from twilio.com/console
+auth_token  = "936f9d5102f38eb51014f68bd0d11c13"
+
+client = Client(account_sid, auth_token)
+
+message = client.messages.create(
+    to="+16073749303", 
+    from_="Twilio_Account number",
+    body="Hello from Python!")
